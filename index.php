8240027dddd87f23e478f560abfdd03906faee06
Keith Becker <keithloganbecker94@gmail.com>
2019-02-01 14:54:29 -0700

    working

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/code/php/Account.php';
-
-
-//\code\php\Account::createAccount('Keith','Becker',NULL,
-//    'keithloganbecker94@gmail.com','8082258615','4240 Swanson Way','Unit 207',
-//    'Castle Rock','80109','CO','US');
-
-
-$account = new \code\php\Account(1);
-echo $account->getFirstName(). ' ' . $account->getLastName()  .', How are you?<BR>';
-$account->setFirstName('Kyle');
-$account = new \code\php\Account(1);
-echo $account->getFirstName(). ' ' . $account->getLastName()  .', How are you?<BR>';
+echo 'Hi!';
 
 
