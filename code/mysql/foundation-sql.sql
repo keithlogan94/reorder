9460f0afc1349dd725f4f83d9b31b7342261a1a5
Keith Becker <keithloganbecker94@gmail.com>
2019-02-01 18:23:34 -0700

    working on upsert_address

--- code/mysql/foundation-sql.sql
+++ code/mysql/foundation-sql.sql
@@ -126,3 +126,214 @@ END $$
 
 
 DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS insert_crm_address;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE insert_crm_address
+  (
+  p_crm_account_id INT(11),
+  p_address_line1 VARCHAR(200),
+  p_address_line2 VARCHAR(200),
+  p_city VARCHAR(100),
+  p_state VARCHAR(100),
+  p_zip VARCHAR(100),
+  p_country VARCHAR(10),
+  p_is_billing BOOLEAN,
+  p_billing_first_name VARCHAR(40),
+  p_billing_last_name VARCHAR(40),
+  p_is_shipping BOOLEAN,
+  p_shipping_first_name VARCHAR(40),
+  p_shipping_last_name VARCHAR(40)
+)
+BEGIN
+
+
+    INSERT INTO crm_address (crm_account_id, street1, street2, city, state, zip, country, billing_first_name,
+                             billing_last_name,is_billing, shipping_first_name, shipping_last_name, is_shipping)
+    VALUES
+    (
+      p_crm_account_id,
+      p_address_line1,
+      p_address_line2,
+      p_city,
+      p_state,
+      p_zip,
+      p_country,
+      p_billing_first_name,
+      p_billing_last_name,
+      p_is_billing,
+      p_shipping_first_name,
+      p_shipping_last_name,
+      p_is_shipping
+    );
+
+    SELECT LAST_INSERT_ID() AS 'last_insert_id';
+
+end $$
+
+DELIMITER ;
+
+
+
+
+DROP PROCEDURE IF EXISTS upsert_crm_address;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE upsert_crm_address
+(
+  p_which_type ENUM('billing','shipping','primary'),
+  p_crm_account_id INT(11),
+  p_address_line1 VARCHAR(200),
+  p_address_line2 VARCHAR(200),
+  p_city VARCHAR(100),
+  p_state VARCHAR(100),
+  p_zip VARCHAR(100),
+  p_country VARCHAR(10),
+  p_first_name VARCHAR(40),
+  p_last_name VARCHAR(40)
+)
+BEGIN
+
+  IF (p_which_type = 'billing') THEN
+    IF NOT EXISTS (SELECT * FROM crm_address WHERE crm_account_id = p_crm_account_id AND
+                                                   start_date < NOW() AND
+      (end_date > NOW() OR end_date IS NULL) AND is_billing = TRUE) THEN
+      INSERT INTO crm_address (crm_account_id, street1, street2, city, state, zip, country, billing_first_name,
+                               billing_last_name,is_billing, shipping_first_name, shipping_last_name, is_shipping)
+      VALUES
+      (
+        p_crm_account_id,
+        p_address_line1,
+        p_address_line2,
+        p_city,
+        p_state,
+        p_zip,
+        p_country,
+        p_first_name,
+        p_last_name,
+        TRUE,
+        NULL,
+        NULL,
+        FALSE
+      );
+
+      ELSE
+      UPDATE crm_address
+        SET
+            street1 = p_address_line1,
+            street2 = p_address_line2,
+            city = p_city,
+            state = p_state,
+            country = p_country,
+            zip = p_zip,
+            billing_first_name = p_first_name,
+            billing_last_name = p_last_name
+      WHERE crm_account_id = p_crm_account_id
+      AND is_billing = TRUE AND start_date < NOW() AND
+        (end_date > NOW() OR end_date IS NULL);
+    end if;
+
+    ELSEIF (p_which_type = 'shipping') THEN
+      IF NOT EXISTS (SELECT * FROM crm_address WHERE crm_account_id = p_crm_account_id AND
+          start_date < NOW() AND
+        (end_date > NOW() OR end_date IS NULL) AND is_shipping = TRUE) THEN
+        INSERT INTO crm_address (crm_account_id, street1, street2, city, state, zip, country, billing_first_name,
+                                 billing_last_name,is_billing, shipping_first_name, shipping_last_name, is_shipping)
+        VALUES
+        (
+          p_crm_account_id,
+          p_address_line1,
+          p_address_line2,
+          p_city,
+          p_state,
+          p_zip,
+          p_country,
+          NULL,
+          NULL,
+          FALSE,
+          p_first_name,
+          p_last_name,
+          TRUE
+        );
+
+      ELSE
+        UPDATE crm_address
+        SET
+          street1 = p_address_line1,
+          street2 = p_address_line2,
+          city = p_city,
+          state = p_state,
+          country = p_country,
+          zip = p_zip,
+          shipping_first_name = p_first_name,
+          shipping_last_name = p_last_name
+        WHERE crm_account_id = p_crm_account_id
+          AND is_shipping = TRUE AND start_date < NOW() AND
+          (end_date > NOW() OR end_date IS NULL);
+      end if;
+
+    ELSEIF (p_which_type = 'primary') THEN
+      IF NOT EXISTS (SELECT * FROM crm_address WHERE crm_account_id = p_crm_account_id AND
+          start_date < NOW() AND
+        (end_date > NOW() OR end_date IS NULL) AND is_billing = FALSE AND is_shipping = FALSE) THEN
+        INSERT INTO crm_address (crm_account_id, street1, street2, city, state, zip, country, billing_first_name,
+                                 billing_last_name,is_billing, shipping_first_name, shipping_last_name, is_shipping)
+        VALUES
+        (
+          p_crm_account_id,
+          p_address_line1,
+          p_address_line2,
+          p_city,
+          p_state,
+          p_zip,
+          p_country,
+          NULL,
+          NULL,
+          FALSE,
+          NULL,
+          NULL,
+          FALSE
+        );
+
+      ELSE
+        UPDATE crm_address
+        SET
+          street1 = p_address_line1,
+          street2 = p_address_line2,
+          city = p_city,
+          state = p_state,
+          country = p_country,
+          zip = p_zip
+        WHERE crm_account_id = p_crm_account_id
+          AND is_billing = FALSE AND is_shipping = FALSE AND start_date < NOW() AND
+          (end_date > NOW() OR end_date IS NULL);
+      end if;
+
+  end if;
+
+end $$
+
+DELIMITER ;
+
+
+
+
+# CALL upsert_crm_address(4,'4240 Swanson Way',NULL,'Castle Rock','CO','80109','US',TRUE,'Keith','Becker',TRUE,'Keith','Becker');
+# SELECT * FROM crm_address;
+
+
+
+
+
+
+
+
+
+
+
